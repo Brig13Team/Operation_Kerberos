b507a6db27de30742fd79355129ad75edba00b61
Thomas Müller <Dorbedo@t-online.de>
2015-05-31 12:12:24 +0200

    New Log system

--- [Dorb]Operation_Kerberos_ACE3.Altis/functions/script_component.hpp
+++ [Dorb]Operation_Kerberos_ACE3.Altis/functions/script_component.hpp
@@ -1,11 +1,16 @@
 #define PREFIX dorb
 #define COMPONENT kerberos
 #define CUSTOM_FOLDER functions
+
+#define DEBUGMODUS
+#define DEBUG_MODE_FULL
+#define DORB_DEBUG
+#define DORB_LOG
+
 #include "\x\cba\addons\main\script_macros_mission.hpp"
 
 #include "script_macros.hpp"
+#include "script_debug.hpp"
 
 
 
-/// DEF
-#define DEBUGMODUS
