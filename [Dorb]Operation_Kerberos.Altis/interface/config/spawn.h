04ddd8af9546060fdda1461e0a716e62afc95e3b
Thomas Müller <Dorbedo@t-online.de>
2015-10-25 06:52:13 +0100

    GUI cleanup and new system

--- [Dorb]Operation_Kerberos.Altis/interface/config/spawn.h
+++ [Dorb]Operation_Kerberos.Altis/interface/config/spawn.h
@@ -30,68 +30,68 @@ class GVAR(veh_spawn) {
 		spawn_listbox
 	};
 	class spawn_CenterBackground: dorb_gui_backgroundBase {
-		x = _get_screen_x(0)+_get_screen_w(0.1);	
-		y = _get_screen_x(0)+_get_screen_h(0.2);
-		w = _get_screen_w(0.46);
-		h = _get_screen_h(0.5);
+		x = GUI_XW(0,0.1);	
+		y = GUI_YH(0,0.2);
+		w = GUI_W(0.46);
+		h = GUI_H(0.5);
 		text = "";
 		colorBackground[] = UI_CL_BODY;
 	};
 	class spawn_RightBackground: spawn_CenterBackground {
-		x = _get_screen_x(0)+_get_screen_w(0.565);	
-		y = _get_screen_x(0)+_get_screen_h(0.305);
-		w = _get_screen_w(0.235);
-		h = _get_screen_h(0.395);
+		x = GUI_XW(0,0.565);	
+		y = GUI_YH(0,0.305);
+		w = GUI_W(0.235);
+		h = GUI_H(0.395);
 	};
 	class spawn_Fahrzeug : spawn_RightBackground {
 		idc = 600103;	
-		y = _get_screen_y(0)+_get_screen_h(0.2);
-		h = _get_screen_h(0.1);
-		sizeEx = _get_screen_h(0.027);
+		y = GUI_YH(0,0.2);
+		h = GUI_H(0.1);
+		sizeEx = GUI_H(0.027);
 		colorText[] = UI_CL_TEXT_DARK;
 		colorBackground[]=UI_CL_HEADER2;
 		text = "";
 	};
 	class spawn_Fahrzeug_pic : dorb_gui_pictureBase {
 		idc = 600104;	
-		x = _get_screen_x(0)+_get_screen_w(0.598125);
-		y = _get_screen_x(0)+_get_screen_h(0.315);
-		w = _get_screen_w(0.16875);
-		h = _get_screen_h(0.225);
+		x = GUI_XW(0,0.598125);
+		y = GUI_YH(0,0.315);
+		w = GUI_W(0.16875);
+		h = GUI_H(0.225);
 		colorText[] = UI_CL_CTRL_GRAU3;
 		colorBackground[]=UI_CL_BODY;
 		text = "data\icon\icon_heli.paa";
 	};
 	class spawn_HeaderBackground: spawn_CenterBackground {
 		idc = 600101;
-		y = _get_screen_y(0)+_get_screen_h(0.1);
-		w = _get_screen_w(0.7);
-		h = _get_screen_h(0.09);
+		y = GUI_YH(0,0.1);
+		w = GUI_W(0.7);
+		h = GUI_H(0.09);
 		text = "";
 		colorText[] = UI_CL_HEADER_TEXT;
 		colorBackground[] = UI_CL_HEADER;
 	};
 	class spawn_listbox : dorb_gui_listboxN {
 		idc = 600102;
-		x = _get_screen_x(0)+_get_screen_w(0.115);	
-		y = _get_screen_y(0)+_get_screen_h(0.215);
-		w = _get_screen_w(0.43);
-		h = _get_screen_h(0.46);
-		rowHeight = _get_screen_h(0.035);
-		sizeEx = _get_screen_h(0.025);
-		columns[] = {0.0,_get_screen_w(0.08),_get_screen_w(0.14),0.0};
+		x = GUI_XW(0,0.115);	
+		y = GUI_YH(0,0.215);
+		w = GUI_W(0.43);
+		h = GUI_H(0.46);
+		rowHeight = GUI_H(0.035);
+		sizeEx = GUI_H(0.025);
+		columns[] = {0.0,GUI_W(0.08),GUI_W(0.14),0.0};
 		onLBSelChanged = QUOTE(_this spawn FUNC(spawn_select););
 		default = true;
 		
 	};
 	class spawn_bttn_moveIn : dorb_gui_button {
 		idc = 600105;
-		x = _get_screen_x(0)+_get_screen_w(0.58);	
-		y = _get_screen_y(0)+_get_screen_h(0.55);
-		w = _get_screen_w(0.205);
-		h = _get_screen_h(0.06);
+		x = GUI_XW(0,0.58);	
+		y = GUI_YH(0,0.55);
+		w = GUI_W(0.205);
+		h = GUI_H(0.06);
 		style = ST_CENTER;
-		sizeEx = _get_screen_h(0.03);
+		sizeEx = GUI_H(0.03);
 		text = ECSTRING(INTERFACE,SPAWN_DRIVER);
 		colorBackground[] = UI_CL_BTN6_foc;
 		colorBackgroundActive[] = UI_CL_BTN6_foc;
@@ -105,7 +105,7 @@ class GVAR(veh_spawn) {
 	};
 	class spawn_bttn_spawn : spawn_bttn_moveIn {
 		idc = 600106;
-		y = _get_screen_y(0)+_get_screen_h(0.615);
+		y = GUI_YH(0,0.615);
 		text = ECSTRING(INTERFACE,SPAWN_EMPTY);
 		colorBackground[] = UI_CL_BTN2_foc;
 		colorBackgroundActive[] = UI_CL_BTN2_foc;
