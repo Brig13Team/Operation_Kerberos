af34af85b550590487a8db33efd304f4dd91c92e
Dorbedo <Dorbedo@t-online.de>
2016-07-09 09:50:40 +0200

    fix

--- [Dorb]Operation_Kerberos.Altis/main/script_macros.hpp
+++ [Dorb]Operation_Kerberos.Altis/main/script_macros.hpp
@@ -53,6 +53,7 @@ Author:
             Clientinit = QUOTE(call compile ('_fnc_scriptName = ''TRIPLES(PREFIX,var1,clientpreinit)''; scriptName _fnc_scriptName;' + preProcessFileLineNumbers 'var1\XEH_ClientPostInit.sqf')); \
         };
     #define GVAR(var1) DOUBLES(ADDON,var1)
+    #define GVARMAIN(var1) DOUBLES(PREFIX,var1)
     #define QGVAR(var1) QUOTE(GVAR(var1))
     #define EGVAR(var1,var2) TRIPLES(PREFIX,var1,var2)
     #define QEGVAR(var1,var2) QUOTE(EGVAR(var1,var2))
