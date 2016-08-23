c52a5f274b030c6efca74f828b62ed295f24fc09
Thomas Müller <Dorbedo@t-online.de>
2016-08-23 23:34:29 +0200

    ACE_bodyBagObject removal
    removing ACE_bodyBagObject after spawning to reduce framedrop

--- [Dorb]Operation_Kerberos.Altis/spawn/XEH_init.hpp
+++ [Dorb]Operation_Kerberos.Altis/spawn/XEH_init.hpp
@@ -10,3 +10,8 @@ class rhsusf_M1083A1P2_B_M2_d_MHQ_fmtv_usarmy {
         init = "(_this select 0) setVariable ['tf_hasRadio',true,true];";
     };
 };
+class ACE_bodyBagObject {
+    class ADDON {
+        init = "if (local (_this select 0)) then {deleteVehicle (_this select 0);};";
+    };
+};
