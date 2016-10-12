b7a40e6e42697cbc480792994f072138e373a022
Thomas Müller <Dorbedo@t-online.de>
2016-10-12 18:16:09 +0200

    modacre new structure

---
+++ [Dorb]Operation_Kerberos.Altis/modace/CfgComponent.hpp
@@ -0,0 +1,17 @@
+/*
+ *  Author: Dorbedo
+ *
+ *  Description:
+ *      the config of the component
+ *
+ */
+#define CBA_OFF
+#include "script_component.hpp"
+
+class ADDON {
+    Events[] = {};
+    class dependecies {
+        CfgPatches = {'ace_main'};
+        CfgComponent = {};
+    };
+};
