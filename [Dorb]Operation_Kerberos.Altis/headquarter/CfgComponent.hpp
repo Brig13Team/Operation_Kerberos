49eb555a7256ab97289ecaec3bb870f177b03076
Thomas Müller <Dorbedo@t-online.de>
2016-11-12 14:48:46 +0100

    updated HQ into new structure

--- [Dorb]Operation_Kerberos.Altis/headquarter/CfgComponent.hpp
+++ [Dorb]Operation_Kerberos.Altis/headquarter/CfgComponent.hpp
@@ -9,6 +9,11 @@
 #include "script_component.hpp"
 
 class DOUBLES(CfgComponent,ADDON) {
+    class clientpostinit {server = 0;};
+    class initpost {};
+    class killed {};
+    class preinit {};
+    class serverpostinit {client = 0;};
     class dependencies {
         CfgPatches[] = {};
         CfgComponents[] = {"mission","spawn"};
