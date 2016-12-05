7ed240cfd10fc1cbdcf26ef2567d6cd9e352a60b
Thomas Müller <Dorbedo@t-online.de>
2016-12-06 00:52:33 +0100

    bug

--- [Dorb]Operation_Kerberos.Altis/database/CfgComponent.hpp
+++ [Dorb]Operation_Kerberos.Altis/database/CfgComponent.hpp
@@ -9,7 +9,6 @@
 #include "script_component.hpp"
 
 class DOUBLES(CfgComponent,ADDON) {
-    class clientpostinit {server = 0;};
     class preinit {};
     class dependencies {
         CfgPatches[] = {"extDB3"};
--- [Dorb]Operation_Kerberos.Altis/database/README.txt
+++ [Dorb]Operation_Kerberos.Altis/database/README.txt
@@ -1,3 +1,2 @@
 this Component is a Wrapper for a database.
-Currently it's made for Arma3RedisClient.
-http://steamcommunity.com/sharedfiles/filedetails/?id=476718276
+Currently it's made for extDB3.
