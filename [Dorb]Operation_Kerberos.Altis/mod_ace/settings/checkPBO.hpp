41633f7a23fddfbda3a5b3e7607086105df38474
Thomas Müller <Dorbedo@t-online.de>
2016-12-06 14:40:48 +0100

    asrai3 V 1.0.0

--- [Dorb]Operation_Kerberos.Altis/mod_ace/settings/checkPBO.hpp
+++ [Dorb]Operation_Kerberos.Altis/mod_ace/settings/checkPBO.hpp
@@ -1,6 +1,6 @@
 
 class ace_common_checkPBOsWhitelist {
-    value = "['cba_diagnostic_enable_perf_loop','cba_diagnostic_logging','cba_cache_disable','cba_diagnostic_disable_xeh_logging','Disable_XEH_Logging','cba_diagnostic_enable_logging','asr_ai3_main','asr_ai3_cfgammo','asr_ai3_cfgvehicles','asr_ai3_cfgweapons','asr_ai3_rhs_c','BWA3_EnablePIPOptics','asr_ai3_rhs_ru_c','asr_ai3_rhs_us_c','st_sthud','st_sthud_usermenu','extdb3']";
+    value = "['cba_diagnostic_enable_perf_loop','cba_diagnostic_logging','cba_cache_disable','cba_diagnostic_disable_xeh_logging','Disable_XEH_Logging','cba_diagnostic_enable_logging','BWA3_EnablePIPOptics','st_sthud','st_sthud_usermenu','extdb3','asr_ai3_bwa3_c','asr_ai3_bwa3_camo_c','asr_ai3_cfgammo','asr_ai3_cfgcamouflage','asr_ai3_cfgvehicles','asr_ai3_cfgweapons','asr_ai3_greenrico','asr_ai3_main','asr_ai3_niarms_c','asr_ai3_rhs_gr_c','asr_ai3_rhs_gr_camo_c','asr_ai3_rhs_ru_c','asr_ai3_rhs_ru_camo_c','asr_ai3_rhs_us_c','asr_ai3_rhs_us_camo_c']";
     typeName = "STRING";
     force = 1;
 };
