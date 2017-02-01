3eddacd5ec1df59a95ee4fb28da803eb842444bb
Dorbedo <Dorbedo@t-online.de>
2017-02-01 18:47:19 +0100

    ACRE_settings (#145)
    * Changed the ACRE-Settings from a sqf file into CBA_settings

--- [Dorb]Operation_Kerberos.Altis/mod_acre/CfgComponent.hpp
+++ [Dorb]Operation_Kerberos.Altis/mod_acre/CfgComponent.hpp
@@ -9,7 +9,9 @@
 #include "script_component.hpp"
 
 class DOUBLES(CfgComponent,ADDON) {
-    class postinit {};
+    class postInit_babel {};
+    class postInit_Basic {};
+    class postInitServer_settings {client = 0;};
     class dependencies {
         CfgPatches[] = {"acre_main"};
         CfgComponents[] = {};
