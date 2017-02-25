253f9bd909019b7a8e2a3e3ee7d4ab947f461c58
Thomas Müller <Dorbedo@t-online.de>
2017-02-25 22:42:15 +0100

    ace settings

--- [Dorb]Operation_Kerberos.Altis/mod_ace/settings/advanced_throwing.hpp
+++ [Dorb]Operation_Kerberos.Altis/mod_ace/settings/advanced_throwing.hpp
@@ -4,5 +4,5 @@ class ace_advanced_throwing_enablePickUpAttached TURN_ON;
 
 
 class ace_advanced_throwing_enable TURN_ON;
-class ace_advanced_throwing_showThrowArc TURN_OFF;
+class ace_advanced_throwing_showThrowArc TURN_ON;
 class ace_advanced_throwing_showMouseControls TURN_ON;
