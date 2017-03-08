9b032ce812060fe9e0913368945b224210e9611c
Thomas Müller <Dorbedo@t-online.de>
2017-03-08 13:12:33 +0100

    typo

--- [Dorb]Operation_Kerberos.Altis/main/script_macros.hpp
+++ [Dorb]Operation_Kerberos.Altis/main/script_macros.hpp
@@ -736,7 +736,7 @@ Author:
 #define IS_HASH(hash) (hash isEqualType locationNull && {(text hash) isEqualTo DORB_HASH_SYS_NAME})
 #define DORB_HASH_SYS_CREATE(VAR) private VAR = [false] call CBA_fnc_createNamespace; \
     VAR setText DORB_HASH_SYS_NAME;
-#define IS_SERIALIZEDHASH(array) (IS_ARRAY(array) && {(count array) > 0} && {IS_STRING((array select 0))} && {(array select 0) isEqalTo DORB_HASH_SYS_NAME})
+#define IS_SERIALIZEDHASH(array) (IS_ARRAY(array) && {(count array) > 0} && {IS_STRING((array select 0))} && {(array select 0) isEqualTo DORB_HASH_SYS_NAME})
 ////-------------------//
 
 /* -------------------------------------------
