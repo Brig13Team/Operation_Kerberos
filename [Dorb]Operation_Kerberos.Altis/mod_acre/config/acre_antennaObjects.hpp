062e10e7f158cae18a88a67f16a4d96f6d5153eb
Dorbedo <Dorbedo@t-online.de>
2017-03-29 16:50:27 +0200

    Acre antenna (#175)
    * external antenna

--- [Dorb]Operation_Kerberos.Altis/mod_acre/config/acre_antennaObjects.hpp
+++ [Dorb]Operation_Kerberos.Altis/mod_acre/config/acre_antennaObjects.hpp
@@ -2,43 +2,41 @@
  *  Author: Dorbedo
  *
  *  Description:
- *      definines the acre vehicles antenna-Offsets
+ *      definines the acre vehicles antenna-positions
  *
  */
 #include "script_component.hpp"
 class acre_antennaObjects {
     class rhs_gaz66_r142_base {
-        antenna = "ACRE_120CM_VHF_TNC";
-        offset = [0,0,47.75];
-        condition ="_this doorPhase 'mast_source' < 0.01 and (speed _this < 1)";
+        position[] = {0,0,47.75};
+        condition ="(_this select 0) doorPhase 'mast_source' == 1";
     };
     class rhs_gaz66_r142_msv : rhs_gaz66_r142_base {};
     class rhs_gaz66_r142_vdv : rhs_gaz66_r142_base {};
     class rhs_gaz66_r142_vmf : rhs_gaz66_r142_base {};
     class rhs_gaz66_r142_vv : rhs_gaz66_r142_base {};
 
-
+    /*
     class Land_TTowerBig_1_F {
-        offset = [0,0,38];
+        position[] = {0,0,38};
         condition = "true";
+        actionPosition[] = {0,0,-18.5};
     };
-    class Land_TTowerBig_1_F {
-        offset = [0,0,46];
+    class Land_TTowerBig_2_F {
+        position[] = {0,0,46};
         condition = "true";
+        actionPosition[] = {0,0,-22.5};
     };
+    */
     class Land_TTowerSmall_2_F {
-        antenna = "ACRE_13IN_UHF_BNC";
-        offset = [0,0,17];
-        condition = "true";
-    };
-    class Land_TTowerSmall_2_F {
-        antenna = "ACRE_13IN_UHF_BNC";
-        offset = [0,0,10];
-        condition = "true";
+        position[] = {0,0,17};
+        actionPosition[] = {0,-1,-8};
     };
     class Land_TTowerSmall_1_F {
-        antenna = "ACRE_13IN_UHF_BNC";
-        offset = [0,0,4];
-        condition = "true";
+        position[] = {0,0,10};
+        actionPosition[] = {0,0,-4};
+    };
+    class Land_TBox_F {
+        position[] = {0,0,4};
     };
 };
