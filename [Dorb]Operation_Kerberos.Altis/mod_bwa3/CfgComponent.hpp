d583ee2917658df25c9c4813a70994a7c02a5282
Thomas Müller <Dorbedo@t-online.de>
2017-04-17 23:16:56 +0200

    disable navipad membershowing

--- [Dorb]Operation_Kerberos.Altis/mod_bwa3/CfgComponent.hpp
+++ [Dorb]Operation_Kerberos.Altis/mod_bwa3/CfgComponent.hpp
@@ -10,6 +10,7 @@
 
 class DOUBLES(CfgComponent,ADDON) {
     class initPost {};
+    class CLientPostInit {server = 0;};
     class dependencies {
         CfgPatches[] = {'BWA3_common'};
         CfgComponents[] = {};
