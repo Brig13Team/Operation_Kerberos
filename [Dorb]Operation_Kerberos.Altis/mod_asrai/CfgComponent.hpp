e6dbbc264daf964f0ffa7adb98c77e91f7fd4f9f
Thomas Müller <Dorbedo@t-online.de>
2017-04-22 11:32:31 +0200

    asrai fix

--- [Dorb]Operation_Kerberos.Altis/mod_asrai/CfgComponent.hpp
+++ [Dorb]Operation_Kerberos.Altis/mod_asrai/CfgComponent.hpp
@@ -11,7 +11,7 @@
 class DOUBLES(CfgComponent,ADDON) {
     class postinit {};
     class dependencies {
-        CfgPatches[] = {'asr_ai3_main','asr_ai3_cfgammo','asr_ai3_cfgvehicles','asr_ai3_cfgweapons'};
+        CfgPatches[] = {'asr_ai3_main'};
         CfgComponents[] = {};
     };
 };
