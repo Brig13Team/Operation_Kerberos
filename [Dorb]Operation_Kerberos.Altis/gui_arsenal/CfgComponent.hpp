7db9264d6d33895e414a0bfde680d70a9eb49197
Thomas Müller <Dorbedo@t-online.de>
2017-04-29 15:28:55 +0200

    arsenal

--- [Dorb]Operation_Kerberos.Altis/gui_arsenal/CfgComponent.hpp
+++ [Dorb]Operation_Kerberos.Altis/gui_arsenal/CfgComponent.hpp
@@ -9,7 +9,7 @@
 #include "script_component.hpp"
 
 class DOUBLES(CfgComponent,ADDON) {
-    version = "2017.04.22";
+    version = "2017.04.23";
     class clientpostinit {server = 0;};
     class postinitServer {client = 0;};
     class preinit {};
