50f893f94a573cde7ca7eb671c361f969e61b4a4
Thomas Müller <Dorbedo@t-online.de>
2017-09-06 23:51:07 +0200

    indexing compositions, new compositions

--- missions/Operation_Kerberos.Altis/composition/config/missioncompositions.hpp
+++ missions/Operation_Kerberos.Altis/composition/config/missioncompositions.hpp
@@ -1,1 +1,130 @@
-//
+class Kerb_Hill3234_Abschussrampe_02_big_Final {
+    type = "scarab";
+    class header {
+        #include "missioncompositions\Kerb_Hill3234_Abschussrampe_02_big_Final\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\Kerb_Hill3234_Abschussrampe_02_big_Final\composition.sqe"
+    };
+};
+
+class Kerb_Hill3234_Abschussrampe_02_Final {
+    type = "scarab";
+    class header {
+        #include "missioncompositions\Kerb_Hill3234_Abschussrampe_02_Final\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\Kerb_Hill3234_Abschussrampe_02_Final\composition.sqe"
+    };
+};
+
+class Kerb_Hill3234_Abschussrampe_02_small_Final {
+    type = "scarab";
+    class header {
+        #include "missioncompositions\Kerb_Hill3234_Abschussrampe_02_small_Final\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\Kerb_Hill3234_Abschussrampe_02_small_Final\composition.sqe"
+    };
+};
+
+class Kerb_Hill3234_Arty {
+    type = "artillery";
+    class header {
+        #include "missioncompositions\Kerb_Hill3234_Arty\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\Kerb_Hill3234_Arty\composition.sqe"
+    };
+};
+
+class mission_artillery_01 {
+    type = "artillery";
+    class header {
+        #include "missioncompositions\mission_artillery_01\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_artillery_01\composition.sqe"
+    };
+};
+
+class mission_artillery_02 {
+    type = "artillery";
+    class header {
+        #include "missioncompositions\mission_artillery_02\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_artillery_02\composition.sqe"
+    };
+};
+
+class mission_artillery_03 {
+    type = "artillery";
+    class header {
+        #include "missioncompositions\mission_artillery_03\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_artillery_03\composition.sqe"
+    };
+};
+
+class mission_device_01 {
+    type = "device";
+    class header {
+        #include "missioncompositions\mission_device_01\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_device_01\composition.sqe"
+    };
+};
+
+class mission_droneheadquarter_01 {
+    type = "dronecommando";
+    class header {
+        #include "missioncompositions\mission_droneheadquarter_01\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_droneheadquarter_01\composition.sqe"
+    };
+};
+
+class mission_generic_01 {
+    type = "%ALL";
+    class header {
+        #include "missioncompositions\mission_generic_01\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_generic_01\composition.sqe"
+    };
+};
+
+class mission_generic_02 {
+    type = "%ALL";
+    class header {
+        #include "missioncompositions\mission_generic_02\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_generic_02\composition.sqe"
+    };
+};
+
+class mission_generic_03 {
+    type = "%ALL";
+    class header {
+        #include "missioncompositions\mission_generic_03\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_generic_03\composition.sqe"
+    };
+};
+
+class mission_generic_04 {
+    type = "%ALL";
+    class header {
+        #include "missioncompositions\mission_generic_04\header.sqe"
+    };
+    class composition {
+        #include "missioncompositions\mission_generic_04\composition.sqe"
+    };
+};
+
