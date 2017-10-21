028cd8685414671f49f1c2b55faff14435d8ab91
Thomas Müller <Dorbedo@t-online.de>
2017-10-21 17:32:33 +0200

    version number in mission included

---
+++ missions/Operation_Kerberos.Altis/main/script_version.hpp
@@ -0,0 +1,4 @@
+#define MAJOR 0
+#define MINOR 19
+#define PATCHLVL 0
+#define BUILD 3
--- missions/Operation_Kerberos.Altis/script_mission.hpp
+++ missions/Operation_Kerberos.Altis/script_mission.hpp
@@ -1,6 +1,6 @@
 #define PREFIX dorb
 
-
+#include "main\script_version.hpp"
 #include "main\script_macros.hpp"
 #include "main\script_debug.hpp"
 
