aa9b8d0fbbf5a3f2f1bf9a290d599d9728759813
Thomas Müller <Dorbedo@t-online.de>
2017-12-20 16:30:47 +0100

    wip

---
+++ tools/releasebuild.py
@@ -0,0 +1,207 @@
+"""Creating Mission pbos"""
+# Author: Dorbedo
+#
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# pylint: disable=W0702
+
+import os
+import sys
+import re
+import subprocess
+import shutil
+import platform
+from distutils.dir_util import copy_tree
+
+######## GLOBALS #########
+MAINPREFIX = "x"
+PREFIX = "dorb_"
+MAINMISSION = "Operation_Kerberos.Altis"
+##########################
+
+def mod_time(path):
+    """Returns the last modified date"""
+    if not os.path.isdir(path):
+        return os.path.getmtime(path)
+    maxi = os.path.getmtime(path)
+    for file in os.listdir(path):
+        maxi = max(mod_time(os.path.join(path, file)), maxi)
+    return maxi
+
+def check_for_changes(addonspath, module, pre):
+    """checks if the a pbo needs to be rebuild"""
+    if not os.path.exists(os.path.join(addonspath, "{}{}.pbo".format(pre, module))):
+        return True
+    return mod_time(os.path.join(addonspath, module)) > mod_time(os.path.join(addonspath, \
+        "{}{}.pbo".format(pre, module)))
+
+def check_for_obsolete_pbos(addonspath, file):
+    """checks if there is an obsolete pbo"""
+    module = file[len(PREFIX):-4]
+    if not os.path.exists(os.path.join(addonspath, module)):
+        return True
+    return False
+
+def get_version(filepath):
+    """update the build version"""
+    versionfile = open(filepath, "r")
+    hpptext = versionfile.read()
+    versionfile.close()
+
+    if hpptext:
+        majortext = re.search(r"#define MAJOR (.*\b)", hpptext).group(1)
+        minortext = re.search(r"#define MINOR (.*\b)", hpptext).group(1)
+        patchtext = re.search(r"#define PATCHLVL (.*\b)", hpptext).group(1)
+        buildtext = re.search(r"#define BUILD (.*\b)", hpptext).group(1)
+
+
+        buildtext = int(buildtext) + 1
+
+        with open(filepath, "w", newline="\n") as file:
+            file.writelines([
+                "#define MAJOR {}\n".format(majortext),
+                "#define MINOR {}\n".format(minortext),
+                "#define PATCHLVL {}\n".format(patchtext),
+                "#define BUILD {}\n".format(buildtext)
+            ])
+        return majortext, minortext, patchtext, buildtext
+
+
+def main():
+    """main"""
+    print("""
+  #####################
+  # Building Kerberos #
+  #####################
+""")
+
+    scriptpath = os.path.realpath(__file__)
+    projectpath = os.path.dirname(os.path.dirname(scriptpath))
+    addonspath = os.path.join(projectpath, "addons")
+    missionspath = os.path.join(projectpath, "missions")
+    workdrivepath = os.path.normpath("P:")
+    temppath = os.path.join(projectpath, "temp")
+
+    releasepath = os.path.normpath(projectpath + "/release/@dorb/addons")
+    if not os.path.exists(releasepath):
+        os.makedirs(releasepath)
+
+    made = 0
+    failed = 0
+    skipped = 0
+    removed = 0
+
+    if platform.system() == "Windows":
+        path_armake = os.path.normpath(projectpath + "/tools/armake_w64.exe")
+    else:
+        path_armake = "armake"
+        workdrivepath = os.path.normpath("/mnt/p/")
+
+    major, minor, patch, build = get_version(os.path.normpath(addonspath \
+        + "/main/script_version.hpp"))
+
+    print("  Version: {}.{}.{}.{}".format(major, minor, patch, build), "\n")
+
+    if not os.path.exists(os.path.normpath(projectpath + \
+        "/release/privatekeys/Kerberos{}.{}.biprivatekey".format(major, minor))):
+        command = path_armake + " keygen -f " + os.path.normpath(projectpath + \
+            "/release/privatekeys/Kerberos{}.{}".format(major, minor))
+        subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+
+    print("  Creating the servermod")
+
+    for file in os.listdir(addonspath):
+        path = os.path.join(addonspath, file)
+        if not os.path.isdir(path):
+            continue
+        if file[0] == ".":
+            continue
+
+        print("  Making {}{} ...".format(PREFIX, file))
+
+        try:
+            command = path_armake + " build -i " + workdrivepath + \
+                " -w unquoted-string" + " -w redefinition-wo-undef" + \
+                " -f " + os.path.normpath(addonspath + "/" + file) + " " + \
+                os.path.normpath(releasepath + "/" + PREFIX + file + ".pbo")
+            subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+        except:
+            failed += 1
+            print("    Failed to make {}{}.".format(PREFIX, file))
+        else:
+            made += 1
+            print("    Successfully made {}{}.".format(PREFIX, file))
+
+    print("  Signing the servermod")
+
+    if not os.path.exists(os.path.normpath(projectpath + "/keys")):
+        os.makedirs(os.path.normpath(projectpath + "/keys"))
+
+
+
+
+
+    print("\n  Creating the mission files")
+
+    if os.path.exists(temppath):
+        shutil.rmtree(temppath, ignore_errors=True)
+
+    shutil.copy(os.path.normpath(addonspath + "/main/script_version.hpp"), \
+        os.path.normpath(missionspath + "/" + MAINMISSION + "/main/script_version.hpp"))
+
+
+    releasepath = os.path.normpath(projectpath + "/release/@dorb/mpmissions")
+    if not os.path.exists(releasepath):
+        os.makedirs(releasepath)
+
+    for file in os.listdir(missionspath):
+        path = os.path.join(missionspath, file)
+        if not os.path.isdir(path):
+            continue
+        if file[0] == ".":
+            continue
+
+        print("  Making {} ...".format(file))
+
+        try:
+            if file == MAINMISSION:
+                command = path_armake + " build -p -i " + workdrivepath + \
+                " -w unquoted-string" + " -w redefinition-wo-undef" + \
+                " -f " + os.path.normpath(missionspath + "/" + file) + " " + \
+                os.path.normpath(releasepath + "/" + file + ".pbo")
+                subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+            else:
+                os.makedirs(temppath)
+                newdir = os.path.normpath(temppath + "/" + file)
+                shutil.copytree(os.path.normpath(missionspath + "/" + MAINMISSION), newdir)
+                copy_tree(os.path.normpath(missionspath + "/" + file), newdir)
+
+                command = path_armake + " build -p -i " + workdrivepath + \
+                " -w unquoted-string" + " -w redefinition-wo-undef" + \
+                " -f " + newdir + " " + \
+                os.path.normpath(releasepath + "/" + file + ".pbo")
+                subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+
+                shutil.rmtree(temppath, ignore_errors=True)
+        except:
+            failed += 1
+            print("    Failed to make {}.".format(file))
+        else:
+            made += 1
+            print("    Successfully made {}.".format(file))
+
+    releasepath = os.path.normpath(projectpath + "release/@dorb")
+
+    shutil.copy(os.path.normpath(projectpath + "/LICENCSE"), \
+        os.path.normpath(releasepath + "/LICENCSE"))
+
+    shutil.copy(os.path.normpath(projectpath + "/README.md"), \
+        os.path.normpath(releasepath + "/README.md"))
+
+    print("\n  Done. \n")
+    print("  Made {}, skipped {}, removed {}, failed to make {}.\n".format(made, \
+        skipped, removed, failed))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
