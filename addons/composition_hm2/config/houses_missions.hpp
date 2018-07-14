a188cfe2ca2168398021f80e0d09477b81511771
Thomas Müller <Dorbedo@t-online.de>
2018-07-15 00:18:08 +0200

    new compositions

--- addons/composition_hm2/config/houses_missions.hpp
+++ addons/composition_hm2/config/houses_missions.hpp
@@ -1,3 +1,135 @@
+class mission_laptop_outpost_v8 {
+    type = "upload";
+    objecttypes[] = {"Land_Laptop_unfolded_F"};
+    class header {
+        #include "houses\mission_laptop_outpost_v8\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_laptop_outpost_v8\composition.sqe"
+    };
+};
+
+class mission_laptop_outpost_v7 {
+    type = "upload";
+    objecttypes[] = {"Land_Laptop_unfolded_F"};
+    class header {
+        #include "houses\mission_laptop_outpost_v7\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_laptop_outpost_v7\composition.sqe"
+    };
+};
+
+class mission_laptop_outpost_v6 {
+    type = "upload";
+    objecttypes[] = {"Land_Laptop_unfolded_F"};
+    class header {
+        #include "houses\mission_laptop_outpost_v6\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_laptop_outpost_v6\composition.sqe"
+    };
+};
+
+class mission_laptop_outpost_v5 {
+    type = "upload";
+    objecttypes[] = {"Land_Laptop_unfolded_F"};
+    class header {
+        #include "houses\mission_laptop_outpost_v5\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_laptop_outpost_v5\composition.sqe"
+    };
+};
+
+class mission_intel_outpost_v8 {
+    type = "intel";
+    objecttypes[] = {"Land_SatellitePhone_F"};
+    class header {
+        #include "houses\mission_intel_outpost_v8\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_intel_outpost_v8\composition.sqe"
+    };
+};
+
+class mission_intel_outpost_v7 {
+    type = "intel";
+    objecttypes[] = {"Land_SatellitePhone_F"};
+    class header {
+        #include "houses\mission_intel_outpost_v7\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_intel_outpost_v7\composition.sqe"
+    };
+};
+
+class mission_intel_outpost_v6 {
+    type = "intel";
+    objecttypes[] = {"Land_SatellitePhone_F"};
+    class header {
+        #include "houses\mission_intel_outpost_v6\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_intel_outpost_v6\composition.sqe"
+    };
+};
+
+class mission_intel_outpost_v5 {
+    type = "intel";
+    objecttypes[] = {"Land_MobilePhone_smart_F"};
+    class header {
+        #include "houses\mission_intel_outpost_v5\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_intel_outpost_v5\composition.sqe"
+    };
+};
+
+class mission_ammobox_outpost_v7 {
+    type = "weaponcache";
+    objecttypes[] = {"Box_IND_WpsSpecial_F"};
+    class header {
+        #include "houses\mission_ammobox_outpost_v8\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_ammobox_outpost_v8\composition.sqe"
+    };
+};
+
+ mission_ammobox_outpost_v7 {
+    type = "weaponcache";
+    objecttypes[] = {"Box_IND_Wps_F"};
+    class header {
+        #include "houses\mission_ammobox_outpost_v7\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_ammobox_outpost_v7\composition.sqe"
+    };
+};
+
+class mission_ammobox_outpost_v6 {
+    type = "weaponcache";
+    objecttypes[] = {"Box_IND_WpsSpecial_F"};
+    class header {
+        #include "houses\mission_ammobox_outpost_v6\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_ammobox_outpost_v6\composition.sqe"
+    };
+};
+
+class mission_ammobox_outpost_v5 {
+    type = "weaponcache";
+    objecttypes[] = {"Box_IND_Wps_F"};
+    class header {
+        #include "houses\mission_ammobox_outpost_v5\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_ammobox_outpost_v5\composition.sqe"
+    };
+};
+
 class mission_pow_house_big_yellow {
     type = "pow";
     objecttypes[] = {"C_scientist_F"};
@@ -61,6 +193,39 @@ class mission_pow_outpost_v4 {
     };
     class composition {
         #include "houses\mission_pow_outpost_v4\composition.sqe"
+    };
+};
+
+class mission_pow_outpost_v5 {
+    type = "pow";
+    objecttypes[] = {"C_journalist_F"};
+    class header {
+        #include "houses\mission_pow_outpost_v5\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_pow_outpost_v5\composition.sqe"
+    };
+};
+
+class mission_pow_outpost_v6 {
+    type = "pow";
+    objecttypes[] = {"C_journalist_F"};
+    class header {
+        #include "houses\mission_pow_outpost_v6\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_pow_outpost_v6\composition.sqe"
+    };
+};
+
+class mission_pow_outpost_v7 {
+    type = "pow";
+    objecttypes[] = {"C_journalist_F"};
+    class header {
+        #include "houses\mission_pow_outpost_v7\header.sqe"
+    };
+    class composition {
+        #include "houses\mission_pow_outpost_v7\composition.sqe"
     };
 };
 
